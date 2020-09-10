f898ba14dc760ef2d242495d984949b635b5214d
shanthisannasi <57353291+shanthisannasi@users.noreply.github.com>
2020-09-10 13:13:15 +0530

    Added file

---
+++ Fibonacci.py
@@ -0,0 +1,19 @@
+def isFibonacci(inputNumber):
+   firstTerm:int=0
+   secondTerm: int=1
+   thirdTerm:int=0
+   while (thirdTerm < inputNumber):
+       thirdTerm = firstTerm + secondTerm
+       firstTerm = secondTerm
+       secondTerm = thirdTerm
+   if(thirdTerm==inputNumber):
+    return "Yes"
+   else:
+    return "No"
+
+inputNumber=int(input("Enter random number:\n"))
+if(isFibonacci(inputNumber) == "Yes"):
+         print (inputNumber,"is a Fibonacci Number")
+else:
+         print (inputNumber,"is a not Fibonacci Number ")
+
